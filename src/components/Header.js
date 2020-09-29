758ed5e1d52da6b0c6c1eb785ed17fd1caae4da1
AndrewFitzpatrick <andrew@weareredshift.com>
2020-09-28 21:45:48 -0400

    add skeleton grid

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,19 +2,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+const navData = [
+  {
+    // fontSize: 'h5',
+    link: '/about',
+    text: 'WHO IS THIS GUY?',
+    classes: 'four columns small'
+  },
+  {
+    // fontSize: 'h4',
+    link: '/',
+    text: 'ANDREW FITZPATRICK',
+    classes: 'four columns large'
+  },
+  {
+    // fontSize: 'h5',
+    link: '/contact',
+    text: 'SAY HI',
+    classes: 'four columns small'
+  }
+];
+
+
 const Header = () => {
   return (
     <header>
       <nav>
-        <Link to="/about">
-          <button>About</button>
-        </Link>
-        <Link to="/">
-          <button>Andrew Fitzpatrick</button>
-        </Link>
-        <Link to="/contact">
-          <button>Contact</button>
-        </Link>
+        <div className="container">
+          <div class="row">
+            {
+              navData.map((item, i) => (
+                <div className = {item.classes} key={i}>
+                  <h4>
+                    <Link to = {item.link} key={i}>
+                      {item.text}
+                    </Link>
+                  </h4>
+                </div>
+              ))
+            }
+          </div>
+        </div>
       </nav>
     </header>
   );
--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
+import './App.scss';
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById("root"));
