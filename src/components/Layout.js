786844266029f749456e8e05dcc69404dd4b62a1
AndrewFitzpatrick <andrew@weareredshift.com>
2020-09-30 12:59:25 -0400

    added react-router-transit-transition

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,13 @@
 // src/components/Layout.js
 import React from "react";
 // import { Link } from "react-router-dom";
-import Header from "./Header.js";
-import Footer from "./Footer.js";
+// import Header from "./Header.js";
+// import Footer from "./Footer.js";
 
 const Layout = ({ children }) => {
   return (
     <div>
-      <Header />
       <main>{children}</main>
-      <Footer />
     </div>
   );
 };
